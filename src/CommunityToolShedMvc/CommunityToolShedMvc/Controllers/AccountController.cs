890f9a09c8c30c899f1cd9de4abbb4aa84c316da
James Churchill <james@smashdev.com>
2019-09-11 09:35:42 -0700

    First pass at Login page

---
+++ src/CommunityToolShedMvc/CommunityToolShedMvc/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolShedMvc.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CommunityToolShedMvc.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            var viewModel = new LoginViewModel();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(LoginViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+    }
+}
