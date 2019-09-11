108175db27b8d90c26f05c6c8ac48d158ea47d61
James Churchill <james@smashdev.com>
2019-09-11 12:11:00 -0700

    Implemented actual auth

--- src/CommunityToolShedMvc/CommunityToolShedMvc/Controllers/AccountController.cs
+++ src/CommunityToolShedMvc/CommunityToolShedMvc/Controllers/AccountController.cs
@@ -1,6 +1,10 @@
-﻿using CommunityToolShedMvc.ViewModels;
+﻿using CommunityToolShedMvc.Data;
+using CommunityToolShedMvc.Models;
+using CommunityToolShedMvc.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -22,9 +26,20 @@ namespace CommunityToolShedMvc.Controllers
         [AllowAnonymous]
         public ActionResult Login(LoginViewModel viewModel)
         {
+            if (ModelState.IsValidField("Email") && ModelState.IsValidField("Password"))
+            {
+                Person person = DatabaseHelper.RetrieveSingle<Person>(@"
+                    select HashedPassword
+                    from Person
+                    where Email = @Email
+                ",
+                    new SqlParameter("@Email", viewModel.Email));
 
-
-
+                if (person == null || !BCrypt.Net.BCrypt.Verify(viewModel.Password, person.HashedPassword))
+                {
+                    ModelState.AddModelError("", "Login failed.");
+                }
+            }
 
             if (ModelState.IsValid)
             {
---
+++ src/CommunityToolShedMvc/CommunityToolShedMvc/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommunityToolShedMvc.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string HashedPassword { get; set; }
+    }
+}
